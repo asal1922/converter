be7a49c1474395bcd5ecd3b371801411d956c8a3
asal <asalrazavi2002@gmail.com>
2023-07-14 05:55:15 +0200

    adding new files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+﻿let c1=document.querySelector('.C');
+let f1=document.querySelector('.F');
+let changeitem=document.querySelector('.changeButton')
+let convertButton=document.querySelector('.convertButton');
+let input=document.getElementById('converter');
+let flag=true;
+let resetButton=document.querySelector('.resetButton');
+changeitem.addEventListener('click',function(){
+    let resultchange=c1.innerHTML;
+    c1.innerHTML=f1.innerHTML;
+    f1.innerHTML=resultchange;
+    
+})
+convertButton.addEventListener('click',function(){
+    if(c1.innerHTML==='°C'){
+        input.value=(input.value*1.8)+32;
+        }
+        else input.value=(input.value-32)/1.8;
+console.log(input.value)        
+})
+resetButton.addEventListener('click',function(){
+    input.value='';
+})
